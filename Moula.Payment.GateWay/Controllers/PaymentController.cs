d294b30e55597f7c5ce2f9bfc28e4ed668773dd8
Cong MA <macong1989@gmail.com>
2020-09-07 20:03:57 +1000

    Add payment controller.

---
+++ Moula.Payment.GateWay/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Moula.Payment.GateWay.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult HealthCheck()
+        {
+            return Ok();
+        }
+    }
+}
